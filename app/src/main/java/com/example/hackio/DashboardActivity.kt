e2021aab92abb17ad87a45a594f0cdeb984050d9
Akshay <akshayrapper7@gmail.com>
2022-01-03 00:54:09 +0530

    App bar Added

--- app/src/main/java/com/example/hackio/DashboardActivity.kt
+++ app/src/main/java/com/example/hackio/DashboardActivity.kt
@@ -2,8 +2,11 @@ package com.example.hackio
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -25,20 +28,61 @@ class DashboardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        navigation_view.setNavigationItemSelectedListener{
+            when (it.itemId) {
+                R.id.nav_home -> {
+                    Toast.makeText(this, "nav home me ho", Toast.LENGTH_SHORT).show()
+                }
+                R.id.akshay -> {
+                    Toast.makeText(this, "akshayyyy", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
+        }
+
+        val drawerToggle = ActionBarDrawerToggle(this, drawer, R.string.open, R.string.close)
+        drawer.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+//        logout.setOnClickListener {
+//            loggout()
+//        }
+    }
+
+    private fun loggout() {
         // call requestIdToken as follows
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build()
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        mGoogleSignInClient.signOut().addOnCompleteListener {
+            val intent = Intent(this, MainActivity::class.java)
+            Toast.makeText(this, "Logging Out", Toast.LENGTH_SHORT).show()
+            startActivity(intent)
+            finish()
+        }
+    }
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
 
-        logout.setOnClickListener {
-            mGoogleSignInClient.signOut().addOnCompleteListener {
-                val intent = Intent(this, MainActivity::class.java)
-                Toast.makeText(this, "Logging Out", Toast.LENGTH_SHORT).show()
-                startActivity(intent)
-                finish()
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        super.onOptionsItemSelected(item)
+        return when (item.itemId) {
+            android.R.id.home -> {
+                drawer.openDrawer(GravityCompat.START)
+                true
             }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
